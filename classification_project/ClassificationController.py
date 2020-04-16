2fb43b9ce49d4a1a639a7cfe2e3970cc196df60e
Oleh Kopylchak <oleg12311999@gmail.com>
2020-04-17 02:35:15 +0300

    Fix cross-origin behaviour. Now it allows to make requests only from specified host

--- classification_project/ClassificationController.py
+++ classification_project/ClassificationController.py
@@ -2,7 +2,6 @@ import json
 import uuid
 
 from flask import Flask, send_file, request, session
-from flask_cors import CORS, cross_origin
 
 from classification_project.ApplicationConstants import ApplicationConstants
 from classification_project.ClassificationRequestDtoMapper import ClassificationRequestDtoMapper
@@ -11,10 +10,8 @@ from classification_project.FileUtils import FileUtils
 from classification_project.Scheduler import Scheduler
 
 app = Flask(__name__)
-app.secret_key = ApplicationConstants.APP_SECRET_KEY.name
-host = ApplicationConstants.CLIENT_HOST.name
-
-cors = CORS(app, resources={r"/foo": {"origins": host}})
+app.secret_key = ApplicationConstants.APP_SECRET_KEY.value
+host = ApplicationConstants.CLIENT_HOST.value
 
 mapper = ClassificationRequestDtoMapper()
 classification_service = ClassificationService()
@@ -25,7 +22,6 @@ scheduler.schedule_clearing_tmp_folder()
 
 
 @app.route('/test-data', methods=['POST'])
-@cross_origin(origin=host, headers=['Content- Type', 'Authorization'])
 def upload_test_data():
     file_utils.save_session_based_txt_file(request.files['file'], 'test_data')
 
@@ -33,7 +29,6 @@ def upload_test_data():
 
 
 @app.route('/train-data', methods=['POST'])
-@cross_origin(origin=host, headers=['Content- Type', 'Authorization'])
 def upload_train_data():
     file_utils.save_session_based_txt_file(request.files['file'], 'train_data')
 
@@ -41,21 +36,18 @@ def upload_train_data():
 
 
 @app.route('/test-data', methods=['GET'])
-@cross_origin(origin=host, headers=['Content- Type', 'Authorization'])
 def is_test_data_for_session():
     return json.dumps({'file_exists': file_utils.is_file_for_session('test_data')}), \
            200, {'ContentType': 'application/json'}
 
 
 @app.route('/train-data', methods=['GET'])
-@cross_origin(origin=host, headers=['Content- Type', 'Authorization'])
 def is_train_data_for_session():
     return json.dumps({'file_exists': file_utils.is_file_for_session('train_data')}), \
            200, {'ContentType': 'application/json'}
 
 
 @app.route('/classification', methods=['POST'])
-@cross_origin(origin=host, headers=['Content- Type', 'Authorization'])
 def classify_and_get_info():
     request_params = mapper.map(request.get_json())
 
@@ -63,7 +55,6 @@ def classify_and_get_info():
 
 
 @app.route('/classification-data', methods=['POST'])
-@cross_origin(origin=host, headers=['Content- Type', 'Authorization'])
 def classify_and_get_data():
     return send_file(classification_service.classify_and_get_data(),
                      attachment_filename='classification_results.zip', as_attachment=True)
@@ -73,6 +64,15 @@ def classify_and_get_data():
 def set_session_id():
     if 'session_id' not in session:
         session['session_id'] = uuid.uuid1()
+
+
+@app.after_request
+def set_cross_origin(response):
+    response.headers['Access-Control-Allow-Origin'] = host
+    response.headers['Access-Control-Allow-Headers'] = host
+    response.headers['Access-Control-Allow-Methods'] = host
+
+    return response
 
 
 if __name__ == '__main__':
