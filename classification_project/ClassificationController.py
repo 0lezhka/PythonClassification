f1f042e62e0484854d4e9cbe3224e7fc06d84d6f
Oleh Kopylchak <oleg12311999@gmail.com>
2020-04-17 01:49:56 +0300

    Add ApplicationCostants enum, that contains application constants. Add cross-origin for posiibility to do request from angular client.

--- classification_project/ClassificationController.py
+++ classification_project/ClassificationController.py
@@ -2,14 +2,19 @@ import json
 import uuid
 
 from flask import Flask, send_file, request, session
+from flask_cors import CORS, cross_origin
 
+from classification_project.ApplicationConstants import ApplicationConstants
 from classification_project.ClassificationRequestDtoMapper import ClassificationRequestDtoMapper
 from classification_project.ClassificationService import ClassificationService
 from classification_project.FileUtils import FileUtils
 from classification_project.Scheduler import Scheduler
 
 app = Flask(__name__)
-app.secret_key = 'e8fd411b86609d1b6416e1e3da69ab27'
+app.secret_key = ApplicationConstants.APP_SECRET_KEY.name
+host = ApplicationConstants.CLIENT_HOST.name
+
+cors = CORS(app, resources={r"/foo": {"origins": host}})
 
 mapper = ClassificationRequestDtoMapper()
 classification_service = ClassificationService()
@@ -20,6 +25,7 @@ scheduler.schedule_clearing_tmp_folder()
 
 
 @app.route('/test-data', methods=['POST'])
+@cross_origin(origin=host, headers=['Content- Type', 'Authorization'])
 def upload_test_data():
     file_utils.save_session_based_txt_file(request.files['file'], 'test_data')
 
@@ -27,6 +33,7 @@ def upload_test_data():
 
 
 @app.route('/train-data', methods=['POST'])
+@cross_origin(origin=host, headers=['Content- Type', 'Authorization'])
 def upload_train_data():
     file_utils.save_session_based_txt_file(request.files['file'], 'train_data')
 
@@ -34,18 +41,21 @@ def upload_train_data():
 
 
 @app.route('/test-data', methods=['GET'])
+@cross_origin(origin=host, headers=['Content- Type', 'Authorization'])
 def is_test_data_for_session():
     return json.dumps({'file_exists': file_utils.is_file_for_session('test_data')}), \
            200, {'ContentType': 'application/json'}
 
 
 @app.route('/train-data', methods=['GET'])
+@cross_origin(origin=host, headers=['Content- Type', 'Authorization'])
 def is_train_data_for_session():
     return json.dumps({'file_exists': file_utils.is_file_for_session('train_data')}), \
            200, {'ContentType': 'application/json'}
 
 
 @app.route('/classification', methods=['POST'])
+@cross_origin(origin=host, headers=['Content- Type', 'Authorization'])
 def classify_and_get_info():
     request_params = mapper.map(request.get_json())
 
@@ -53,6 +63,7 @@ def classify_and_get_info():
 
 
 @app.route('/classification-data', methods=['POST'])
+@cross_origin(origin=host, headers=['Content- Type', 'Authorization'])
 def classify_and_get_data():
     return send_file(classification_service.classify_and_get_data(),
                      attachment_filename='classification_results.zip', as_attachment=True)
